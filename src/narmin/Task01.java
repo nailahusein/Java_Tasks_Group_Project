89abed457f9759cf2d5feea5298c1db9a9a3894c
nmekh <nmekhralieva@gmail.com>
2024-03-23 20:36:54 -0700

    Task 01, 02, 03 are completed

---
+++ src/narmin/Task01.java
@@ -0,0 +1,24 @@
+package narmin;
+
+public class Task01 {
+
+     /*
+      QUESTION 1 - Write a method which can identify if a given number odd or even
+       Ex: identify(5) -->"Odd"
+       identify (6) --> "Even"
+      */
+
+        public static void main(String[] args) {
+            int number= 5;
+
+            if(number%2==0){
+                System.out.println("its ever");
+            } else
+                System.out.println("its odd");
+        }
+
+
+
+
+
+    }
