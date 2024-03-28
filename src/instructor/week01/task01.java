0034a3f28f5b2717b115d2ba9b88e9dfb2b5336f
Dina <dcojocaru3@gmail.com>
2024-03-28 11:24:30 -0700

    solved

--- src/instructor/week01/task01.java
+++ src/instructor/week01/task01.java
@@ -1,6 +1,6 @@
 
 
-package instructor;public class task01 {
+package instructor.week01;public class task01 {
      /*
       QUESTION 1 - Write a method which can identify if a given number odd or even
        Ex: identify(5) -->"Odd"
