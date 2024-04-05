a1adf879462d00e6be50a7dfe3510eedf12a2638
nailahusein <naile3690@gmail.com>
2024-04-05 12:22:25 -0700

    Naila's branch

--- src/narmin/week02/T2_NumbersDivisibleBy.java
+++ src/narmin/week02/T2_NumbersDivisibleBy.java
@@ -5,10 +5,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class T2_NumbersDivisibleBy {
-    public static void main(String[] args) {
 
 
-        class DivisibleNumbers {
             public static void main(String[] args) {
                 Scanner scanner = new Scanner(System.in);
                 System.out.print("Enter a number: ");
@@ -34,7 +32,6 @@ public class T2_NumbersDivisibleBy {
                 System.out.println("Divisible By 3: " + divisibleBy3);
             }
         }
-    }
 
     //Numbers – Divisible by 3, 5, 15
     //Write a program that can print the numbers between 1 ~ N that can be divisible by 3, 5, and 15.
@@ -57,4 +54,4 @@ public class T2_NumbersDivisibleBy {
     //
 
 
-}
+
