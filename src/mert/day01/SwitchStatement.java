5adb074ada001327613babf3cd4b08d5d5b4ce9f
Mert <merterimyild@gmail.com>
2024-04-05 13:12:38 -0700

    commit

---
+++ src/mert/day01/SwitchStatement.java
@@ -0,0 +1,24 @@
+package mert.day01;
+
+public class SwitchStatement {
+}
+
+/*
+Create a class called Calculator. Given two double variables named n1 and n2 and a char
+variable named mathOperator, write a program that calculates the result of n1 and n2 based on
+the given math operator.
+
+		char operator -> -, +, *, /
+
+		When the operator is +: add n1, n2
+		When the operator is -: subtract n1, n2
+		When the operator is *: multiply n1, n2
+		When the operator is /: divide n1, n2
+		For any other operators, print "invalid operator."
+
+            Example:
+                  n1 = 10, n2 = 20, mathOperator = '+'
+
+            Output:
+                  30
+ */
---
+++ src/mert/week02/W2_SwapTwoVariables.java
@@ -0,0 +1,9 @@
+package mert.week02;
+
+public class W2_SwapTwoVariables {
+}
+/*
+Numbers – Swap Numbers
+Swap two variables' values without using a third variable.
+
+*/
