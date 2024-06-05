85560ca3729ab1889fd8742fd227e30a1c0f18d0
nmehralieva <nmehralieva@gmail.com>
2024-06-05 07:47:36 -0700

    Updated tasks week 09 task01

---
+++ src/narmin/week09/ArrayConcat.java
@@ -0,0 +1,37 @@
+package narmin.week09;
+
+public class ArrayConcat {
+
+    public static int[] concatArrays(int[] array1, int[] array2) {
+
+        int totalLength = array1.length + array2.length;
+
+
+        int[] result = new int[totalLength];
+
+
+        for (int i = 0; i < array1.length; i++) {
+            result[i] = array1[i];
+        }
+
+
+        for (int i = 0; i < array2.length; i++) {
+            result[array1.length + i] = array2[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {4, 5, 6};
+
+        int[] concatenatedArray = concatArrays(array1, array2);
+
+
+        for (int element : concatenatedArray) {
+            System.out.print(element + " ");
+        }
+    }
+}
