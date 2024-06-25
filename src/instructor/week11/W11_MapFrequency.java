77c7721380bfd41aa1d383a6c02b5509b4edee01
OwenEST <owen.constantine@cydeo.com>
2024-06-25 19:05:36 +0300

    Week 11 Solutions

--- src/instructor/week11/W11_MapFrequency.java
+++ src/instructor/week11/W11_MapFrequency.java
@@ -1,7 +1,147 @@
 package instructor.week11;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 //Write a method that prints the frequency of each character from a String
 public class W11_MapFrequency {
+    public static void main(String[] args) {
+        String test = "AaronBaron";
+        FrequencyTest(test);
+    }
+
+    public static void FrequencyTest(String  str ) {
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        for (Character each : str.toCharArray()) {  // taking each character
+            if (map.containsKey(each)) { //  if it contains
+                map.put(each, map.get(each) + 1);
+            } else {
+                map.put(each, 1);
+            }
+        }
+        System.out.println(map);
+    }
+
 
 
 }
+/*public class MapUniqueCharString {
+    public static void main(String[] args) {
+
+        String str = "accabbcd";
+        Map<Character, Integer> unique = new HashMap<>();
+
+        for( int j = 0; j < str.length(); j++) {
+            int count =0;
+            for (int i = 0; i < str.length(); i++) {
+                if(str.charAt(i) == str.charAt(j))
+                    count++;
+            }
+            if( count == 1){
+                unique.put(str.charAt(j), count);
+            }
+        }
+
+        System.out.println(unique);
+
+    }
+}*/
+
+/*public static Map<String,Integer> frequencyOfChar(String str){ //creating the method
+
+        Map<String,Integer> map = new LinkedHashMap<>(); //creating a linkedhashmap object
+
+        for (int i = 0; i < str.length(); i++) { //first loop to go through the characters
+            int count = 0; // variable count for frequency of the characters
+            for (int j = 0; j < str.length(); j++) { //2nd loop to compare the characters next to each other
+                if (str.charAt(i)== str.charAt(j)) { //if the elements at index i match the elements at index j
+                    count++; //then increase their count
+                }
+                map.put((""+str.charAt(i)), count); // each character, their count
+
+            }
+
+        }
+        return map;
+
+    }*/
+
+/*public class Map_FrequencyOfCharacter {
+  public static void main(String[] args) {
+    String           str="aaaccbbbbcccc"; FrequencyOfCharacter(str); }
+
+  public static void FrequencyOfCharacter(String str){ Map<Character,Integer> map=new LinkedHashMap<>();
+    for(Character each:str.toCharArray()){  // taking each character
+      if (map.containsKey(each)){  //  if it contains
+            map.put(each,map.get(each)+1);
+      }else{ map.put(each,1);
+    }
+  }
+System.out.println(map); }
+}*/
+
+
+/*public static void main(String[] args) {
+
+        String str = "database";
+        System.out.println(freqOfChars(str));
+
+    }
+
+    public static Map<String,Integer> freqOfChars(String str){
+
+        Map<String,Integer> map = new LinkedHashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+            for (int i1 = 0; i1 < str.length(); i1++) {
+                if (str.charAt(i) == str.charAt(i1)){
+                    count++;
+                }
+                map.put(""+str.charAt(i),count);
+            }
+
+        }
+        return map;
+    }
+}*/
+    /*
+public static void frequency(String str ) {
+
+    Map<Character, Integer> map = new LinkedHashMap<>();
+
+    for (Character each : str.toCharArray()) {
+        if (!map.containsKey(each)) {
+            map.put(each, 1);
+        } else {
+            map.put(each, map.get(each) + 1);
+        }
+    }
+    System.out.println(map);
+}
+
+*/
+
+/*public static void main(String[] args) {
+
+        frequencyOfChars("stadghdkjkkl");
+
+    }
+
+
+    public static void frequencyOfChars(String str) {
+
+        Map<String, Integer> map = new LinkedHashMap<>();
+
+       //char[] chars = str.toCharArray();
+
+        for (String each : str.split("")) {
+            int frequency = Collections.frequency(Arrays.asList(str.split("")), each);
+
+            map.put(each, frequency);
+        }
+
+        System.out.println(map);
+    }
+
+}*/
--- src/instructor/week11/W11_MapMinValue.java
+++ src/instructor/week11/W11_MapMinValue.java
@@ -1,11 +1,180 @@
 package instructor.week11;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 //Write a method that can return the minimum value from ta map (DO NOT use sort method)
 public class W11_MapMinValue {
 
 
+    public static void main(String[] args) {
+        Map<String, Integer> test= new HashMap<>();
+        test.put("Aaron", 11);
+        test.put("Baron", 5);
+        test.put("Cydeo", 27);
+        test.put("Canada", 100);
+        System.out.println(minValue(test));
+    }
 
+
+    public  static  int  minValue( Map<String,Integer> map  ) {
+        SortedSet<Integer> sm = new TreeSet<>(map.values());
+        return sm.first();
+    }
 
 
 
 }
+/*//Write a method that can return the minimum value from ta map (DO NOT use sort method)
+public class mapMinValue {
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 4);
+        map.put("B", 3);
+        map.put("C", 2);
+        map.put("D", 1);
+
+        System.out.println(minValue(map));
+
+    }
+
+    public  static  int  minValue( Map<String,Integer> map  ) {
+        SortedSet<Integer> sm = new TreeSet<>(map.values());
+        return sm.first( );
+    }
+}
+*/
+
+/*public static Integer getMinValue(Map<String, Integer> map) {
+        Integer minValue = null;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            Integer value = entry.getValue();
+            if (minValue == null || value.compareTo(minValue) < 0) {
+                minValue = value;
+            }
+        }
+        return minValue;
+    }*/
+
+/*public static int minValue(Map<String, Integer> map){
+
+        Set<Integer> set = new TreeSet<>(); //no duplicates, ascending sorted order
+
+        for (Map.Entry<String, Integer> eachSet : map.entrySet()) {
+        //for each loop to get each key and each value
+            set.add(eachSet.getValue()); //adding those keys and values into the set
+        }
+
+        for (Integer each : set) { //getting each of the integers from that set
+            return each;
+
+        }
+        return 0;
+
+    }*/
+/*public static Map.Entry<String, Integer> minValue2(Map<String,Integer> map){
+
+        Map.Entry<String, Integer> min = null;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (min == null || min.getValue() > entry.getValue()) {
+                min = entry;
+            }
+        }
+
+        System.out.println(min.getKey()); // 0.1
+
+
+
+        return min;
+    }*/
+
+/*public static Map.Entry<String,Integer> minValue3(Map<String,Integer> map){
+        return Collections.min(map.entrySet(),
+                Map.Entry.comparingByValue());
+
+
+    }*/
+
+/*public static Integer getMinValue(Map<String, Integer> map) {
+        Integer minValue = null;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            Integer value = entry.getValue();
+            if (minValue == null || value.compareTo(minValue) < 0) {
+                minValue = value;
+            }
+        }
+        return minValue;
+    }*/
+/*public static int minValue(Map<String, Integer> map){ Set<Integer> set = new TreeSet<>();
+for (Map.Entry<String, Integer> eachSet : map.entrySet()) { set.add(eachSet.getValue());
+}
+int a= 0;
+for (Integer each : set) {
+a= each; break;
+}
+return a; }*/
+
+/*public static void main(String[] args) {
+
+        Map<String,Integer> map = new LinkedHashMap<>();
+        map.put("age1", 16);
+        map.put("age2", 20);
+        map.put("age3", 18);
+        System.out.println(minValue(map));
+    }
+
+    public static int minValue(Map<String,Integer> map){ // method takes "Map<String,Integer>" and returns min value of Map
+
+        Set<Integer> set = new TreeSet<>(); //creating a "TreeSet<>()" named set to store the values of Map
+
+        for (Map.Entry<String, Integer> eachSet : map.entrySet()) { // iterates each entry in the input Map
+            set.add(eachSet.getValue()); // and adds the value to the set
+        }
+        return Collections.min(set); // calling the min method of the Collections and passes set as argument
+        // then min method returns the smallest element in the set
+    }
+}
+
+// the code finds the minimum value by creating a TreeSet of its values then using "Collections.min" it finds smallest elemen  */
+
+
+/*public static String minimum(Map<String,Integer> map){
+
+        String name = "";
+        int min = Integer.MAX_VALUE; // by default this is the min value
+
+        for (Map.Entry<String, Integer> eachEntry : map.entrySet()) { // loop through the Map
+            if (eachEntry.getValue() < min) { // get each value and compare with the min value
+                 min = eachEntry.getValue();  // if less than current min assign to the min
+                 name = eachEntry.getKey();   // get the key value
+            }
+        }
+        return name + " with " + min + " $ "; // return the min value
+    }*/
+
+/*public static void main(String[] args) {
+
+        Map<String, Integer> map = Map.of("A", 5,
+                                          "B", 3,
+                                          "C", 7,
+                                          "D", 1);
+        System.out.println("minValue(map) = " + minValue(map));
+    }
+
+
+    public static int minValue(Map<String, Integer> map){
+
+        int min = Integer.MAX_VALUE; //to ensure that 1st value is always <=min
+
+        for(Integer eachValue : map.values()){
+            if(eachValue < min){
+                min = eachValue;
+            }
+        }
+        return min;
+
+
+    }
+}*/
